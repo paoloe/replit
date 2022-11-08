cb1deffdce6b1323e8a12037ae3b875622a4ceb9
paoloespiritu <paoloespiritu@outlook.com>
2022-11-08 22:45:21 +0000

    FizzBuzz

--- myjs.js
+++ myjs.js
@@ -7,3 +7,19 @@ function capitalize(string){
 function lastLetter(string1){
 	return string1.slice(-1);
 }
+
+function fizzbuzz(){
+	let answer = parseInt(document.getElementById('input_value').value);
+
+	for (let i = 1; i <= answer; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("FizzBuzz");
+		  } else if (i % 3 === 0) {
+			console.log("Fizz");
+		  } else if (i % 5 === 0) {
+			console.log("Buzz");
+		  } else {
+			console.log(i);
+		  }
+	}
+}
